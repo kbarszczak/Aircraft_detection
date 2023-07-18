fed488ebac23f7c5203d3672b976e78e2c21f291
kbarszczak <kamilbarszczak62@gmail.com>
2023-07-18 15:42:14 +0200

    Added finished dataset.py

--- detection/models/yolo_v1/dataset.py
+++ detection/models/yolo_v1/dataset.py
@@ -7,11 +7,11 @@ import cv2
 
 class YoloDataset(torch.utils.data.Dataset):
     def __init__(self, path: str, subdir: str, filename: str, labels: str, shape: tuple[int, int, int] = (160, 160, 3),
-                 cell_count=5, cell_boxes=2,
+                 cell_count=5, cell_boxes=1,
                  classes=43, **kwargs):
         super(YoloDataset, self).__init__(**kwargs)
 
-        assert cell_boxes == 2, f"Cell boxes for value {cell_boxes} is not implemented"
+        assert cell_boxes == 1, f"Cell boxes for value {cell_boxes} is not implemented"
 
         self.path = os.path.join(path, subdir)
         self.shape = shape
@@ -57,21 +57,19 @@ class YoloDataset(torch.utils.data.Dataset):
             ymin /= height_coef
             ymax /= height_coef
 
-            x_center = ((xmin + xmax) / 2.0) / self.shape[1]
-            y_center = ((ymin + ymax) / 2.0) / self.shape[0]
+            xcenter = ((xmin + xmax) / 2.0) / self.shape[1]
+            ycenter = ((ymin + ymax) / 2.0) / self.shape[0]
             width = (xmax - xmin) / self.shape[1]
             height = (ymax - ymin) / self.shape[0]
 
-            x, y = int(x_center * self.cell_count), int(y_center * self.cell_count)
-            x_cell, y_cell = x_center * self.cell_count - x, y_center * self.cell_count - y
-            width_cell, height_cell = width * self.cell_count, height * self.cell_count
+            x, y = int(xcenter * self.cell_count), int(ycenter * self.cell_count)
+            xcell, ycell = xcenter * self.cell_count - x, ycenter * self.cell_count - y
+            widthcell, heightcell = width * self.cell_count, height * self.cell_count
+
+            if boxes[y, x, self.classes] == 0:
+                boxes[y, x, self.classes] = 1.0
+                boxes[y, x, class_index] = 1.0
+                boxes[y, x, self.classes + 1:self.classes + 5] = torch.tensor([xcell, ycell, widthcell, heightcell],
+                                                                              dtype=torch.float32)
 
         return boxes
-
-
-d = torch.utils.data.DataLoader(YoloDataset('D:/Data/Aircraft_Detection', 'dataset', 'train.txt', "labels.csv",
-                                            (5 * 2 * 2 * 2 * 2 * 2, 5 * 2 * 2 * 2 * 2 * 2, 3)), batch_size=10,
-                                shuffle=True)
-b = next(iter(d))
-print(b[1][0])
-print(b[1].shape)
