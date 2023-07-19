Added callbacks. Fixed minor issues such as: logger bug, history calculation bug, model state saving bug and training bug. Added mse and mae metrics

detection/components/callbacks/callback.py
@@ -1,7 +1,13 @@
 from abc import abstractmethod
+import matplotlib.pyplot as plt
 import numpy as np
 import os
 import torch
+import torch.nn as nn
+import torch.optim as optim
+import pickle
+from tqdm import tqdm
+import cv2
 
 
 class Callback:
@@ -40,15 +46,23 @@ class PerTrainingCallback(Callback):
         pass
 
 
-class SaveStateCallback(PerStepCallback):
-    def __init__(self, target_path, frequency=500, **kwargs):
-        super(SaveStateCallback, self).__init__(**kwargs)
+class SaveNetStateCallback(PerStepCallback):
+    def __init__(self, target_path, prefix: str = 'yolo', per_epoch_frequency=2, **kwargs):
+        super(SaveNetStateCallback, self).__init__(**kwargs)
+
+        assert per_epoch_frequency >= 1, "Per epoch frequency has to be at least 1"
+
         self.target_path = target_path
-        self.frequency = frequency
+        self.per_epoch_frequency = per_epoch_frequency
+        self.prefix = prefix
         self.best_loss = None
+        self.frequency = None
 
     def __call__(self, args):
-        if args[1] % self.frequency != 0:
+        if self.frequency is None:
+            self.frequency = int(args[5] / self.per_epoch_frequency) - 1
+
+        if args[4] <= 1 or args[4] % self.frequency != 0:
             return
 
         model = args[0]
@@ -58,6 +72,212 @@ class SaveStateCallback(PerStepCallback):
         loss = loss_fn.mean('train')
 
         if self.best_loss is None or self.best_loss > loss:
-            filename = os.path.join(self.target_path, f'yolo_l={loss}_e={epoch + 1}_s={step + 1}.pt')
+            filename = os.path.join(self.target_path,
+                                    (f'{self.prefix}_l=%.4f_e=%3d_s=%6d.pt' % (loss, epoch + 1, step + 1)).replace(' ',
+                                                                                                                   '0'))
             torch.save(model.state_dict(), filename)
             self.best_loss = loss
+
+
+class SaveHistoryPlotsCallback(PerTrainingCallback):
+    def __init__(self, target_path, configs: tuple = (
+            ("history.png", None, False, None),
+            ("history_normalized.png", None, True, None)
+    ), **kwargs):
+        super(SaveHistoryPlotsCallback, self).__init__(**kwargs)
+        self.target_path = target_path
+        self.configs = configs
+
+    def __call__(self, args):
+        print("[Callback] Saving history plots ...")
+        for config in self.configs:
+            SaveHistoryPlotsCallback._save_history_plot(os.path.join(self.target_path, config[0]), args[3], config[1],
+                                                        config[2], config[3])
+
+    @staticmethod
+    def _save_history_plot(path: str, history: dict, include: list[str] | None, use_norm: bool, steps: int | None,
+                           fig_size: tuple[int] = (10, 5)):
+        def compress_loss(loss, steps):
+            return [np.average(loss[steps * i:steps * (i + 1)]) for i in range(len(loss) // steps)]
+
+        def norm(data):
+            return (data - np.min(data)) / (np.max(data) - np.min(data))
+
+        def gcd(a, b):
+            while b:
+                a, b = b, a % b
+            return a
+
+        plt.clf()
+        fig = plt.figure(figsize=fig_size)
+
+        history = history.copy()
+        if include is not None:
+            history = {metric: values for metric, values in history.items() if metric in include}
+
+        for metric, values in history.items():
+            # skip val
+            if "val" in metric:
+                continue
+
+            # make values equal size
+            val_values = history[f'val_{metric}']
+            if len(val_values) != len(values):
+                gdc = gcd(len(val_values), len(values))
+                values = compress_loss(values, len(values) // gdc)
+                val_values = compress_loss(val_values, len(val_values) // gdc)
+
+            if use_norm:
+                pivot = len(values)
+                data = norm(values + val_values)
+                values = data[pivot:]
+                val_values = data[:pivot]
+
+            if steps is not None:
+                values = compress_loss(values, steps)
+                val_values = compress_loss(val_values, steps)
+
+            history[metric] = values
+            history[f'val_{metric}'] = val_values
+
+        colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'w']
+        for index, (metric, values) in enumerate(history.items()):
+            if "val" in metric:
+                continue
+
+            steps = range(1, len(values) + 1)
+            plt.plot(steps, values, color=colors[index], linestyle='-', marker='o', label=metric)
+            plt.plot(steps, history[f'val_{metric}'], color=colors[index], linestyle='--', marker='o', label=f"val_{metric}")
+
+        plt.title("Comparision of training and validating scores")
+        plt.xlabel('Steps')
+        plt.ylabel("Values" if not use_norm else "Values normalized")
+        plt.legend(loc='upper left')
+        plt.grid(True, axis='y')
+        plt.savefig(path)
+        plt.close(fig)
+
+
+class SaveHistoryCallback(PerTrainingCallback):
+    def __init__(self, target_path, filename: str = 'history.pickle', **kwargs):
+        super(SaveHistoryCallback, self).__init__(**kwargs)
+        self.target_path = target_path
+        self.filename = filename
+
+    def __call__(self, args):
+        print("[Callback] Saving history ...")
+        with open(os.path.join(self.target_path, self.filename), 'wb') as file:
+            pickle.dump(args[3], file, pickle.HIGHEST_PROTOCOL)
+
+
+class SaveModelCallback(PerTrainingCallback):
+    def __init__(self, target_path, model, filename: str = 'yolo.pt', **kwargs):
+        super(SaveModelCallback, self).__init__(**kwargs)
+        self.target_path = target_path
+        self.model = model
+        self.filename = filename
+
+    def __call__(self, args):
+        print("[Callback] Saving model ...")
+        torch.save(self.model.state_dict(), os.path.join(self.target_path, self.filename))
+
+
+class VisualizePytorchCNNLayersCallback(PerTrainingCallback):
+    def __init__(self, target_path, model, device, height, width, channels, margin=3, steps=10, lr=0.1,
+                 include_nested=True, **kwargs):
+        super(VisualizePytorchCNNLayersCallback, self).__init__(**kwargs)
+
+        assert margin >= 0, "Margin cannot be negative"
+        assert steps > 0, "Steps has to be positive"
+
+        self.target_path = target_path
+        self.model = model
+        self.device = device
+        self.height = height
+        self.channels = channels
+        self.width = width
+        self.margin = margin
+        self.steps = steps
+        self.lr = lr
+        self.include_nested = include_nested
+
+    def __call__(self, args):
+        print("[Callback] Visualizing PyTorch CNNs ...")
+
+        activations = {}
+
+        def hook_fun(model, input, output):
+            activations['activation'] = output
+
+        self.model.train(False)
+        queue = list(self.model.named_children())
+        while queue:
+            name, layer = queue.pop(0)
+            if self.include_nested:
+                queue.extend([(f'{name}_{n}', l) for n, l in layer.named_children()])
+
+            if isinstance(layer, nn.Conv2d) or isinstance(layer, nn.ConvTranspose2d):
+                f_count = layer.out_channels
+                rows, cols = VisualizePytorchCNNLayersCallback._rows_cols(f_count)
+                result = np.zeros(
+                    (rows * self.height + (rows - 1) * self.margin, cols * self.width + (cols - 1) * self.margin, 3),
+                    dtype='uint8')
+
+                for index in tqdm(range(rows * cols)):
+                    i, j = index // cols, index % cols
+                    filter_index = j + (i * cols)
+
+                    hook = layer.register_forward_hook(hook_fun)
+                    noise = (np.random.rand(1, self.channels, self.height, self.width) * 0.2 + 0.4).astype('float32')
+                    tensor = torch.from_numpy(noise).to(self.device).requires_grad_(True)
+                    optimizer = optim.NAdam([tensor], lr=self.lr)
+
+                    for step in range(self.steps):
+                        optimizer.zero_grad()
+                        _ = self.model(tensor)
+                        activation = activations['activation'][:, filter_index, :, :].unsqueeze(dim=1)
+                        loss = torch.mean(activation)
+                        loss.backward()
+                        optimizer.step()
+
+                    tensor = tensor.detach()[0].permute((1, 2, 0))
+                    filter_img = VisualizePytorchCNNLayersCallback._deprocess_image(tensor.cpu().numpy())
+                    result = VisualizePytorchCNNLayersCallback._append_image(result, filter_img, i, j, self.margin,
+                                                                             self.height, self.width)
+                    hook.remove()
+
+                cv2.imwrite(os.path.join(self.target_path, f'{name}.png'), result)
+
+    @staticmethod
+    def _deprocess_image(img):
+        img -= img.mean()
+        img /= img.std() + 1e-5
+        img *= 0.15
+
+        img += 0.5
+        img = np.clip(img, 0, 1)
+
+        img *= 255
+        img = np.clip(img, 0, 255).astype("uint8")
+
+        return img
+
+    @staticmethod
+    def _append_image(image, append_image, row, col, margin, height, width):
+        horizontal_start = row * height + row * margin
+        horizontal_end = horizontal_start + height
+        vertical_start = col * width + col * margin
+        vertical_end = vertical_start + width
+        image[horizontal_start: horizontal_end, vertical_start: vertical_end, :] = append_image
+        return image
+
+    @staticmethod
+    def _rows_cols(value):
+        assert value >= 1
+        rows, cols = 1, value
+        for i in range(2, value // 2 + 1):
+            if value % i == 0:
+                if np.abs(i - int(value / i)) < np.abs(rows - cols):
+                    rows = i
+                    cols = int(value / i)
+        return rows, cols

detection/components/metrics/metric.py
@@ -59,7 +59,7 @@ class PytorchMetric(Metric):
 
 
 class PytorchYoloV1Loss(PytorchMetric):
-    def __init__(self, name: str, cell_count=5, cell_boxes=2, classes=43, **kwargs):
+    def __init__(self, name: str = 'loss', cell_count=5, cell_boxes=2, classes=43, **kwargs):
         super(PytorchYoloV1Loss, self).__init__(name, **kwargs)
 
         assert cell_boxes == 2, f"Cell boxes for value {cell_boxes} is not implemented"
@@ -123,3 +123,23 @@ class PytorchYoloV1Loss(PytorchMetric):
         )
 
         return self.lambda_coord * box_coord_loss + obj_loss + self.lambda_noobj * no_obj_loss + class_loss
+
+
+class PytorchMseMetric(PytorchMetric):
+    def __init__(self, name: str = 'mse', **kwargs):
+        super(PytorchMseMetric, self).__init__(name, **kwargs)
+        self.mse = nn.MSELoss()
+
+    @abstractmethod
+    def __call__(self, y_true: torch.Tensor, y_pred: torch.Tensor):
+        return self.mse(y_true, y_pred)
+
+
+class PytorchMaeMetric(PytorchMetric):
+    def __init__(self, name: str = 'mae', **kwargs):
+        super(PytorchMaeMetric, self).__init__(name, **kwargs)
+        self.mae = nn.L1Loss()
+
+    @abstractmethod
+    def __call__(self, y_true: torch.Tensor, y_pred: torch.Tensor):
+        return self.mae(y_true, y_pred)

detection/models/yolo_v1/dataset.py
@@ -2,7 +2,6 @@ import torch
 import numpy as np
 import pandas as pd
 import os
-import cv2
 
 
 class YoloDataset(torch.utils.data.Dataset):
@@ -26,7 +25,7 @@ class YoloDataset(torch.utils.data.Dataset):
 
     def __getitem__(self, idx):
         filename = str(self.ids.iloc[idx, 0])
-        image_filename = f'{filename}.jpg'
+        image_filename = f'{filename}.bin'
         image_b_boxes = f'{filename}.csv'
 
         image = self._load_process_image(os.path.join(self.path, image_filename))
@@ -35,32 +34,15 @@ class YoloDataset(torch.utils.data.Dataset):
         return image, b_boxes
 
     def _load_process_image(self, path):
-        img = cv2.cvtColor(cv2.imread(path), cv2.COLOR_BGR2RGB)
-        img = cv2.resize(img, (self.shape[1], self.shape[0]), interpolation=cv2.INTER_LINEAR)
-        return torch.from_numpy(np.transpose((img / 255.0).astype('float32'), (2, 0, 1)))
+        with open(path, 'rb') as bf:
+            return torch.from_numpy(np.reshape(np.frombuffer(bf.read(), dtype='float32'), (self.shape[2], self.shape[0], self.shape[1])).copy())
 
     def _load_process_b_boxes(self, path):
         boxes = torch.zeros((self.cell_count, self.cell_count, (self.cell_boxes * 5 + self.classes)),
                             dtype=torch.float32)
         for _, row in pd.read_csv(path).iterrows():
-            class_index = self.labels.loc[self.labels['classes'] == row['class'], 'indices'].values[0]
-
-            width, height, xmin, xmax, ymin, ymax = row['width'], row['height'], row['xmin'], row['xmax'], row['ymin'], \
-                                                    row['ymax']
-
-            width_coef = width / self.shape[1]
-            height_coef = height / self.shape[0]
-
-            xmin /= width_coef
-            xmax /= width_coef
-
-            ymin /= height_coef
-            ymax /= height_coef
-
-            xcenter = ((xmin + xmax) / 2.0) / self.shape[1]
-            ycenter = ((ymin + ymax) / 2.0) / self.shape[0]
-            width = (xmax - xmin) / self.shape[1]
-            height = (ymax - ymin) / self.shape[0]
+            class_index = row['class_index']
+            width, height, xcenter, ycenter = row['width'], row['height'], row['xcenter'], row['ycenter']
 
             x, y = int(xcenter * self.cell_count), int(ycenter * self.cell_count)
             xcell, ycell = xcenter * self.cell_count - x, ycenter * self.cell_count - y

detection/models/yolo_v1/provider.py
@@ -5,13 +5,14 @@ import detection.models.model as m
 import detection.models.yolo_v1.model as yolo
 import detection.models.yolo_v1.dataset as dataset
 import detection.components.metrics.metric as metric
+import detection.components.callbacks.callback as callback
 
 
 class YoloProvider(provider.Provider):
     def __init__(self, **kwargs):
         super(YoloProvider, self).__init__(**kwargs)
 
-    def get_model(self, device: str, target_path: str) -> type(m.Model):
+    def get_model(self, device: str, target_path: str, input_shape: tuple) -> type(m.Model):
         if device == 'cuda' or device == 'cpu':
             device = torch.device(device)
         else:
@@ -20,60 +21,94 @@ class YoloProvider(provider.Provider):
         model = yolo.YoloModel().to(device)
         pytorch_model = m.PytorchModel(
             model=model,
-            loss=metric.PytorchYoloV1Loss("loss"),
-            metrics=[],
-            callbacks=[],
+            loss=metric.PytorchYoloV1Loss(),
+            metrics=[
+            ],
+            callbacks=[
+                callback.SaveModelCallback(
+                    target_path=target_path,
+                    model=model,
+                    filename='yolo_v1.pt'
+                ),
+                callback.SaveHistoryCallback(
+                    target_path=target_path,
+                    filename='history.pickle'
+                ),
+                callback.SaveNetStateCallback(
+                    target_path=target_path,
+                    prefix='yolo',
+                    per_epoch_frequency=1
+                ),
+                callback.SaveHistoryPlotsCallback(
+                    target_path=target_path,
+                    configs=(
+                        ("history.png", None, False, None)
+                    )
+                ),
+                callback.VisualizePytorchCNNLayersCallback(
+                    target_path=target_path,
+                    model=model,
+                    device=device,
+                    height=input_shape[0],
+                    width=input_shape[1],
+                    channels=input_shape[2],
+                    margin=3,
+                    steps=10,
+                    lr=0.1,
+                    include_nested=True
+                )
+            ],
             optimizer=torch.optim.NAdam(model.parameters(), lr=1e-4),
             device=device
         )
 
         return pytorch_model
 
-    def get_data(self, data_path: str, train_file: str, test_file: str, valid_file: str, batch_size: int, width: int,
-                 height: int) -> (object, object, object):
+    def get_data(self, data_path: str, train_file: str, test_file: str, valid_file: str, batch_size: int,
+                 input_shape: tuple) -> (object, object, object):
         train = torch.utils.data.DataLoader(
             dataset=dataset.YoloDataset(
                 path=data_path,
-                subdir='dataset',
+                subdir='dataset_processed',
                 filename=train_file,
                 labels='labels.csv',
-                shape=(height, width, 3)
+                shape=input_shape
             ),
             shuffle=True,
             batch_size=batch_size,
             drop_last=True,
-            prefetch_factor=50,
-            num_workers=3
+            prefetch_factor=10,
+            num_workers=2
         )
 
         test = torch.utils.data.DataLoader(
             dataset=dataset.YoloDataset(
                 path=data_path,
-                subdir='dataset',
+                subdir='dataset_processed',
                 filename=test_file,
                 labels='labels.csv',
-                shape=(height, width, 3)
+                shape=input_shape
             ),
             shuffle=False,
             batch_size=batch_size,
             drop_last=True,
-            prefetch_factor=50,
-            num_workers=3
+            prefetch_factor=10,
+            num_workers=2
         )
 
         valid = torch.utils.data.DataLoader(
             dataset=dataset.YoloDataset(
                 path=data_path,
-                subdir='dataset',
+                subdir='dataset_processed',
                 filename=valid_file,
                 labels='labels.csv',
-                shape=(height, width, 3)
+                shape=input_shape
             ),
             shuffle=False,
             batch_size=batch_size,
             drop_last=True,
-            prefetch_factor=50,
-            num_workers=3
+            prefetch_factor=10,
+            num_workers=2
         )
 
         return train, test, valid